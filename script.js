775e6233ffe5462ee2f5cb47c6384bb7279c58b6
himaisogin <98646029+himais0gin@users.noreply.github.com>
2023-05-31 07:32:52 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const board = document.getElementById('board');
+  const cells = [];
+
+  // ボードのセルを生成
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cells.push(cell);
+      board.appendChild(cell);
+    }
+  }
+
+  // ゲームの初期配置を設定
+
+  // ピースを置く関数
+  function placePiece(cell, color) {
+    const piece = document.createElement('div');
+    piece.classList.add('piece', color);
+    cell.appendChild(piece);
+  }
+
+  // クリックイベントのハンドラ
+  function handleCellClick(event) {
+    const cell = event.target;
+    // ピースを置く処理
+
+    // 盤面の更新
+
+    // ターンの切り替え
+
+    // ゲームの終了判定
+
+  }
+
+  // セルにクリックイベントを追加
+  cells.forEach((cell) => {
+    cell.addEventListener('click', handleCellClick);
+  });
+
+  // リセットボタンのハンドラ
+  const resetButton = document.getElementById('reset-btn');
+  resetButton.addEventListener('click', () => {
+    // ゲームをリセットする処理
+
+  });
+
+  // ゲームの初期化
+
+});
